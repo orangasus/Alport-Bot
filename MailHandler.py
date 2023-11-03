ae5064ac98003e3ab0bcbcb6933483c6434379c2
Gleb <73142250+orangasus@users.noreply.github.com>
2023-11-03 02:17:15 +0200

    Updating launch conditions, optimizing email sending

--- MailHandler.py
+++ MailHandler.py
@@ -1,12 +1,11 @@
-import ssl
 import smtplib
-from email.mime.text import MIMEText
+import ssl
 from email.mime.multipart import MIMEMultipart
-from pprint import pprint
+from email.mime.text import MIMEText
 
 from jinja2 import Template
 
-
+# Formats article info for plain text email version
 def format_article_for_email(article):
     result = '{}\n{}\n\n'.format(article['title'], article['date'])
     for el in article['abstract']:
@@ -15,6 +14,7 @@ def format_article_for_email(article):
     result += 'DOI: {}\n{}\n\n\n\n'.format(article['doi'], article['link'])
     return result
 
+# Creates text version of email
 def create_plain_text_email_content(formated_articles, name):
     num = len(formated_articles)
     intro = f'Hi, {name}! Here are some new papers for you that were published on PubMed during last month. Today I have {num} articles for you. Hope you find them interesting!\n\n\n'
@@ -23,6 +23,8 @@ def create_plain_text_email_content(formated_articles, name):
         body += format_article_for_email(article)
     return body
 
+
+# Creates html version of email
 def create_html_email_content(formated_articles, name):
     num = len(formated_articles)
     file = open('template.html')
@@ -30,7 +32,9 @@ def create_html_email_content(formated_articles, name):
     msg = tem.render(formated_articles=formated_articles, num=num, name=name)
     return msg
 
-def send_report(formated_articles, name, email, email_sender, email_password):
+
+# Creates a digest email message for user
+def get_report(email_sender, email, formated_articles, name):
     num = len(formated_articles)
     subject = 'Latest Articles from PubMed'
 
@@ -47,7 +51,20 @@ def send_report(formated_articles, name, email, email_sender, email_password):
     message.attach(plain_version)
     message.attach(html_version)
 
+    return message
+
+# Creates all emails for all users and sends them
+def send_all_reports(user_report_info, email_sender, email_password):
+    addressee_total = len(user_report_info)
+    emails_sent = 0
     ctx = ssl.create_default_context()
     with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=ctx) as smtp:
         smtp.login(email_sender, email_password)
-        smtp.sendmail(email_sender, email, message.as_string())
+        for el in user_report_info:
+            try:
+                message = get_report(email_sender, el[0], el[1], el[2])
+                smtp.sendmail(email_sender, el[0], message.as_string())
+                emails_sent += 1
+            except:
+                print(f'Unable to send email to: {el[0]}')
+    print(f'Emails sent: {emails_sent} out of {addressee_total}')
--- main.py
+++ main.py
@@ -1,34 +1,20 @@
-#TODO:
-# Try to make it async wherever possible
-# Change email address
-# Email Susie: mentioning org. name, list of emails, excel sheet?
-# Upload to server until the 31st
-# Upload to Git
-
-
-from pprint import pprint
+# Don't forget to change relative paths to absolute ones, when uploading to server
 
 # Module for working with date
 import datetime
 
-# Modules for environment variables
-from pathlib import Path
-from dotenv import load_dotenv
-import os
-
 # Modules for working with Google services
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-
 # Module for working with data
 import pandas as pd
-
+from oauth2client.service_account import ServiceAccountCredentials
 from tqdm import tqdm
 
 # My modules
 import MailHandler
 import PubMedHandler
 
+# Modules for environment variables
 
 # Setting up google API client
 scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
@@ -36,8 +22,12 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name('google_cred.json
 google_client = gspread.authorize(credentials)
 
 # Connecting to Google sheets db
-db = google_client.open("AlportBot DB")
+db = google_client.open("AlportBot DB Test")
 sheet1 = db.worksheet('Sheet1')
+
+# Stores all information that will be used to send emails to users
+# user_report_info = [(email, formated_articles, name), ...]
+user_report_info = []
 
 
 def get_formated_current_date():
@@ -56,85 +46,74 @@ def get_user_info_from_row(row):
     return {'name': name, 'email': email, 'key_words': key_words, 'query': query, 'auth_limit': auth_limit}
 
 
-def is_end_of_month():
-    dt = datetime.datetime.now()
-    todays_month = dt.month
-    tomorrows_month = (dt + datetime.timedelta(days=1)).month
-    return tomorrows_month != todays_month
+def get_prev_month_str():
+    return (datetime.date.today() - datetime.timedelta(days=3)).strftime("%b-%d-%Y").split('-')[0]
 
 
 if __name__ == '__main__':
-    if True:
-        # gets current date
-        now = get_formated_current_date()
+    # Gets current year
+    cur_year = str(datetime.datetime.now().year)
+    # Gets previous month in text format e.g. Oct
+    prev_month_str = get_prev_month_str()
 
-        # counts number of sent emails just in case
-        emails_sent = 0
+    # counts number of sent emails just in case
+    emails_sent = 0
 
-        # Loads enviroment variables from .env file
-        current_dir = Path(__file__).resolve().parent if "__file__" in locals() else Path.cwd()
-        envars = current_dir / ".env"
-        load_dotenv(envars)
+    # Reads email credentials
+    with open('email_cred.txt') as cred_txt:
+        email_sender, email_password = cred_txt.readline().split(', ')
 
-        # Reads environment variables
-        email_sender = os.getenv('email_sender')
-        email_password = os.getenv('email_password')
+    # Reads db google sheet
+    df = pd.DataFrame(sheet1.get_all_records())
+    for _, row in tqdm(df.iterrows()):
+        user_data = get_user_info_from_row(row)
 
-        # Reads db google sheet
-        df = pd.DataFrame(sheet1.get_all_records())
-        for _, row in tqdm(df.iterrows()):
-            user_data = get_user_info_from_row(row)
+        # fetches articles
+        id_list = PubMedHandler.search(user_data['query'])['IdList']
 
-            # fetches articles
-            id_list = PubMedHandler.search(user_data['query'])['IdList']
+        articles_fetch = PubMedHandler.fetch_articles_details(id_list)
+        articles_sum = PubMedHandler.get_articles_summary_info(id_list)
 
-            articles_fetch = PubMedHandler.fetch_articles_details(id_list)
-            articles_sum = PubMedHandler.get_articles_summary_info(id_list)
+        formated_articles = []
+        month_before, month_after = None, None
+        n = len(id_list)
+        index = 1
 
-            # formats fetched articles
-            formated_articles = []
-            month_before, month_after = None, None
-            n = len(id_list)
-            index = 1
-            for i, paper in enumerate(articles_fetch['PubmedArticle']):
-                if i < n - 1:
-                    month_before = PubMedHandler.get_formated_pubdate(articles_sum[i + 1])[1]
+        # formats fetched articles
+        for i, paper in enumerate(articles_fetch['PubmedArticle']):
+            if i < n - 1:
+                month_before = PubMedHandler.get_formated_pubdate(articles_sum[i + 1])[1]
 
-                d = PubMedHandler.format_article(articles_sum[i], paper, id_list[i], index, user_data['auth_limit'])
-                # print(d['auth_list'][0])
-                # print('--------------------------')
+            d = PubMedHandler.format_article(articles_sum[i], paper, id_list[i], index, user_data['auth_limit'])
 
-                paper_day, paper_month, paper_year = PubMedHandler.get_formated_pubdate(articles_sum[i])
+            paper_day, paper_month, paper_year = PubMedHandler.get_formated_pubdate(articles_sum[i])
 
-                if paper_month == 'Unknown' and i != 0:
-                    if month_after == month_before:
+            # Sometimes PubMed leaves the month field blank, this code assigns a correct value if that's the case
+            if paper_month == 'Unknown' and i != 0:
+                if month_after == month_before:
+                    paper_month = month_after
+                elif month_after != month_before:
+                    if month_after != 'Unknown':
                         paper_month = month_after
-                    elif month_after != month_before:
-                        if month_after != 'Unknown':
-                            paper_month = month_after
-                        elif month_before != 'Unknown':
-                            paper_month = month_before
+                    elif month_before != 'Unknown':
+                        paper_month = month_before
 
-                if paper_year == now[2] and now[1] in paper_month:
-                    try:
-                        paper_keyword_list = list(paper['MedlineCitation']['KeywordList'][0])
-                    except:
-                        paper_keyword_list = []
+            # Checks if article was published in previous month
+            # Then checks if users keywords are present in article's keyword list
+            if paper_year == cur_year and prev_month_str == paper_month:
+                try:
+                    paper_keyword_list = list(paper['MedlineCitation']['KeywordList'][0])
+                except:
+                    paper_keyword_list = []
 
-                    paper_keyword_list = [str(el).lower() for el in paper_keyword_list]
+                paper_keyword_list = [str(el).lower() for el in paper_keyword_list]
 
-                    if any(x in user_data['key_words'] for x in paper_keyword_list):
-                        formated_articles.append(d)
-                        index += 1
+                if any(x in user_data['key_words'] for x in paper_keyword_list):
+                    formated_articles.append(d)
+                    index += 1
 
-                month_after = paper_month
+            month_after = paper_month
 
-            # Sends report
-            try:
-                MailHandler.send_report(formated_articles, user_data['name'], user_data['email'],
-                                        email_sender, email_password)
-            except Exception as e:
-                print(e)
-            else:
-                emails_sent += 1
-        print(f"Sent Emails: {emails_sent}")
+        user_report_info.append((user_data['email'], formated_articles, user_data['name']))
+
+    MailHandler.send_all_reports(user_report_info, email_sender, email_password)
